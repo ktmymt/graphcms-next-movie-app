83bb8e5fd47ee6baa4043a93a269fcf4bf0784f5
ktmymt <kota.miyamoto18@gmail.com>
2021-11-25 23:03:46 +0900

    add env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,43 @@
-const Home = () => {
+import { gql, GraphQLClient } from "graphql-request"
+import { GetServerSideProps, NextPage } from "next"
+
+interface Props {
+  videos: any
+}
+
+const Home: NextPage<Props> = (props) => {
+  console.log(props.videos)
   return <div>Home</div>
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const url = process.env.ENDPOINT
+  const graphqlClient = new GraphQLClient(url, {
+    headers: {
+      Authorization: process.env.GRAPH_CMS_TOKEN,
+    },
+  })
+
+  const query = gql`
+    query {
+      videos {
+        id
+        title
+        description
+        seen
+        slug
+        tags
+      }
+    }
+  `
+
+  const data = await graphqlClient.request(query)
+
+  return {
+    props: {
+      videos: data.videos,
+    },
+  }
 }
 
 export default Home
